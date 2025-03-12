9f508cea3d5f900bd19732bd280b9dba7c62ad91
Ramon Costa <ramoncostapatel@gmail.com>
2025-03-12 14:36:32 -0700

    pls work

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "fd7a10bcce15be4715aef5777f08b2b2
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "33cc83533e0c6ea1222a5e1982a7d862",
+"flutter_bootstrap.js": "6fd768cd41ed7c0cd12ef54bc254ff02",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
